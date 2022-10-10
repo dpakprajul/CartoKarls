d53abca2f1964e7b60a6ed6702d5820a692d361d
binda <bindaaspratiks@gmail.com>
2022-10-10 15:11:14 +0200

    added value from break.js

--- js/main.js
+++ js/main.js
@@ -22,11 +22,48 @@ window.onload = function () {
         $("#var2").selectmenu({
             change: function (event, ui) {
                 cloro = $("#var2").val(); //change cloropleth value
-                
-                
+                layerHandler();
+                if (cloro == "D02" || cloro == "D03" || cloro == "D05" || cloro == "D06" || cloro == "D07" || cloro == "D08" || cloro == "D09") {
+                    $("#leg1").val("Less than " + (Math.round(breaks[1] * 1000) / 10) + "%");
+                    $("#leg7").val((Math.round((breaks[6] + .001) * 1000) / 10) + " % or more");
+                    for (var i = 2; i < 7; i++) {
+                        $("#leg" + String(i)).val((Math.round((breaks[i - 1] + .001) * 1000) / 10) + " - " + (Math.round(breaks[i] * 1000) / 10) + "%");
+                    }
+                }
+               
+
             }
         }).selectmenu("menuWidget")
             .addClass("overflow");
+        $("var1").selectmenu({
+            change: function (event, ui) {
+                circle = $("#var1").val(); // change circle marker value
+                layerHandler();
+                $("leg00").value("No Data");
+                if (Cbreaks == CwhStu || Cbreaks == ChiStu || Cbreaks == CblStu || Cbreaks == ThiStu || Cbreaks == TblStu || Cbreaks == TwhStu || Cbreaks == Tlunch) {
+                    $("#leg11").val("Less than " + (Math.round(Cbreaks[1] * 1000) / 10) + "%");
+                    $("#leg17").val((Math.round((Cbreaks[6] + .001) * 1000) / 10) + " % or more");
+                    for (var i = 2; i < 7; i++) {
+                        $("#leg1" + String(i)).val((Math.round((Cbreaks[i - 1] + .001) * 1000) / 10) + " - " + (Math.round((Cbreaks[i] * 1000) / 10)) + "%");
+                    }
+                }
+                
+    
+            }
+        }).selectmenu("menuWidget")
+            .addClass("overflow");
+
+        for (var i = 2; i < 7; i++) {
+            $("#leg" + String(i)).val((Math.round((breaks[i - 1] + .001) * 1000) / 1000) + " - " + breaks[i]);
+        }
+        for (var i = 2; i < 7; i++) {
+            $("#leg1" + String(i)).val((Math.round((Cbreaks[i - 1] + .001) * 1000) / 1000) + " - " + Cbreaks[i]);
+        }
+        $("#leg1").val("Less than " + breaks[1]);
+        $("#leg11").val("Less than " + Cbreaks[1]);
+        $("#leg7").val((Math.round((breaks[6] + .001) * 1000) / 1000) + " or more");
+        $("#leg17").val((Math.round((Cbreaks[6] + .001) * 1000) / 1000) + " or more");
+
     });
 }
 
